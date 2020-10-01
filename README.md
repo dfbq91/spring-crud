90e49ce1abd9485a33e0ac7a47ab08b716e332ed
Diego Betancourt <dfbq91@gmail.com>
2020-10-01 17:53:25 -0500

    Added DELETE, PUT and .jar file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-Spring Crud
+# Spring CRUD
+
+A simple CRUD application created with Spring.
+
+## Installation
+
+* Clone the project: git clone https://github.com/dfbq91/spring-crud/
+* Import to your IDE
+* Execute the main method from Application class
+
+## Execution with Maven
+
+* Clone th project: git clone https://github.com/dfbq91/spring-crud/
+* cd spring-crud
+* mvn spring-boot:run
+
+## Using .jar file
+
+The built .jar file is auto-runnable, so as you can move it to any computer having java installed and run the app.
+
+* Clone the project: git clone https://github.com/dfbq91/spring-crud/
+* cd spring-crud
+* java -jar target/spring-0.0.1-SNAPSHOT.jar
+
+## Endpoints
+
+* GET http://localhost:8080/api/v1/person/
+* POST http://localhost:8080/api/v1/person/ { "name": "Lucho Montes" }
+* GET http://localhost:8080/api/v1/person/{_id}
+* PUT http://localhost:8080/api/v1/person/{_id} { "name": "Lucho Figueroa" }
+* DELETE http://localhost:8080/api/v1/person/{_id}
--- src/main/java/com/learning/spring/dao/FakePersonDataAccessService.java
+++ src/main/java/com/learning/spring/dao/FakePersonDataAccessService.java
@@ -29,16 +29,30 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return DB.stream().filter(person -> )
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
     }
 
     @Override
     public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()) return 0;
+        DB.remove(personMaybe.get());
         return 0;
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        return 0;
+    public int updatePersonById(UUID id, Person newPersonData) {
+        return selectPersonById(id)
+                .map(personToUpdate -> {
+                    int indexOfPersonToUpdate= DB.indexOf(personToUpdate);
+                    if (indexOfPersonToUpdate >= 0) {
+                        DB.set(indexOfPersonToUpdate, new Person(id, newPersonData.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 }
